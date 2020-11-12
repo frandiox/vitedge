6dc9ced94511ca974eb767ef503e2cc644826dfb
Fran Dios <frankdiox@gmail.com>
2020-11-13 03:34:13 +0900

    Support edge cache for page rendering

--- core/worker/render.js
+++ core/worker/render.js
@@ -1,10 +1,16 @@
 import router from '__vitedge_router__'
+import { getCachedResponse, setCachedResponse } from './cache'
 import { getPageProps } from './props'
 import { createResponse } from './utils'
 
 export async function handleViewRendering(event) {
-  const page = await getPageProps(event)
+  const cacheKey = event.request.url
+  const cachedResponse = await getCachedResponse(cacheKey)
+  if (cachedResponse) {
+    return cachedResponse
+  }
 
+  const page = await getPageProps(event)
   const initialState = page ? await page.response.json() : {}
 
   const { html } = await router.render({
@@ -18,6 +24,8 @@ export async function handleViewRendering(event) {
       'content-type': 'text/html;charset=UTF-8',
     },
   })
+
+  setCachedResponse(event, response, cacheKey, (page.options.cache || {}).html)
 
   return response
 }
