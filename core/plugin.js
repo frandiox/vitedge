c245ca6a7117384941baa36c88cd5b8abd41eb02
Fran Dios <frankdiox@gmail.com>
2020-11-15 01:43:37 +0900

    Add dev-server to vitedge plugin

--- core/plugin.js
+++ core/plugin.js
@@ -5,4 +5,53 @@ export default {
   alias: {
     vitedge: 'vitedge/entry-client',
   },
+  configureServer: [
+    async ({
+      root, // project root directory, absolute path
+      app, // Koa app instance
+      server, // raw http server instance
+      watcher, // chokidar file watcher instance
+    }) => {
+      async function handleFunctionRequest(ctx, functionPath) {
+        try {
+          const filepath = root + '/functions' + functionPath + '.js'
+          let result = await import(filepath)
+
+          if (result) {
+            result = result.default || result
+            if (result.handler) {
+              ctx.body = await result.handler({
+                request: ctx.request,
+                query: ctx.query,
+                body: ctx.request.body,
+              })
+
+              return
+            }
+          }
+        } catch (error) {
+          console.error(error)
+          ctx.message = error.message
+          ctx.status = 500
+          return
+        }
+
+        ctx.status = 404
+      }
+
+      app.use(async (ctx, next) => {
+        if (ctx.url.includes('/api/')) {
+          await handleFunctionRequest(ctx, ctx.url)
+          return
+        }
+
+        if (ctx.url.includes('/props/')) {
+          await handleFunctionRequest(ctx, ctx.query.propsGetter)
+          return
+        }
+
+        await next()
+      })
+    },
+  ],
 }
