78f69629aa0d8f86f306f123cf5a78e06575e9d8
Fran Dios <frankdiox@gmail.com>
2020-11-19 21:11:05 +0900

    Patch Vite until PR is merged

--- core/bin/cli.js
+++ core/bin/cli.js
@@ -1,11 +1,36 @@
 #!/usr/bin/env node
 
+import path from 'path'
+import { promises as fs } from 'fs'
+import { createRequire } from 'module'
+
 const [, , command, ...args] = process.argv
+
+const patchVite = async () => {
+  const require = createRequire(import.meta.url)
+  const vitePath = path.dirname(require.resolve('vite/package.json'))
+  const viteConfigLoader = path.resolve(vitePath, 'dist/node/config.js')
+
+  try {
+    await fs.writeFile(
+      viteConfigLoader,
+      (await fs.readFile(viteConfigLoader, 'utf8')).replace(
+        /\srequire\(resolvedPath\)/,
+        ' await import(resolvedPath); config = config.default || config'
+      )
+    )
+  } catch (error) {
+    console.warn('Vitedge could not patch Vite:', error.message)
+  }
+}
 
 ;(async () => {
   if (command === 'build') {
     const { default: build } = await import('vitedge/build/index.js')
     await build()
+    process.exit()
+  } else if (command === 'patch') {
+    await patchVite()
     process.exit()
   } else {
     console.log(`Command "${command}" not supported`)
