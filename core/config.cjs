9a00564e7e5cff9203adf1233c7e4fb8df224f89
Fran Dios <frankdiox@gmail.com>
2020-11-26 02:30:40 +0900

    Find parent dir with Vite config and detect TS

--- core/config.cjs
+++ core/config.cjs
@@ -1,17 +1,30 @@
 const fs = require('fs')
 const path = require('path')
 
-const viteConfigName = 'vite.config.js'
+const systemRoot = path.parse(process.cwd()).root
+const fileExists = (dir, file) => fs.existsSync(path.resolve(dir, file))
 
-let rootDir = process.cwd()
-if (!fs.existsSync(path.resolve(rootDir, viteConfigName))) {
-  rootDir = path.resolve(process.cwd(), '..')
-  if (!fs.existsSync(path.resolve(rootDir, viteConfigName))) {
-    throw new Error(`Could not find ${viteConfigName}`)
+let rootDir
+let currentDir = process.cwd()
+let isTS = false
+
+while (!rootDir && currentDir !== systemRoot) {
+  if (fileExists(currentDir, 'vite.config.js')) {
+    rootDir = currentDir
+  } else if (fileExists(currentDir, 'vite.config.ts')) {
+    isTS = true
+    rootDir = currentDir
+  } else {
+    currentDir = path.resolve(currentDir, '..')
   }
 }
 
+if (!rootDir) {
+  throw new Error(`Could not find Vite config file`)
+}
+
 module.exports = {
+  isTS,
   rootDir,
   outDir: 'dist',
   clientOutDir: 'client',
