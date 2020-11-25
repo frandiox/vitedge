4ef9857d52bb61398d109200a45a122a33457d17
Fran Dios <frankdiox@gmail.com>
2020-11-26 01:50:41 +0900

    Simplify rootDir config to support ts extension

--- core/config.cjs
+++ core/config.cjs
@@ -1,18 +1,8 @@
 const fs = require('fs')
 const path = require('path')
 
-const viteConfigName = 'vite.config.js'
-
-let rootDir = process.cwd()
-if (!fs.existsSync(path.resolve(rootDir, viteConfigName))) {
-  rootDir = path.resolve(process.cwd(), '..')
-  if (!fs.existsSync(path.resolve(rootDir, viteConfigName))) {
-    throw new Error(`Could not find ${viteConfigName}`)
-  }
-}
-
 module.exports = {
-  rootDir,
+  rootDir: process.cwd(),
   outDir: 'dist',
   clientOutDir: 'client',
   ssrOutDir: 'ssr',
