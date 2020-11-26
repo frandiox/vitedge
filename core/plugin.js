7ae654e7d573e1422dea9ffccbf276314509807a
Fran Dios <frankdiox@gmail.com>
2020-11-27 03:50:58 +0900

    Perf: Bust ESM cache only when files change

--- core/plugin.js
+++ core/plugin.js
@@ -12,11 +12,19 @@ export default {
       server, // raw http server instance
       watcher, // chokidar file watcher instance
     }) => {
+      const cacheBust = new Map()
+      watcher.on('change', (fullPath) => {
+        if (fullPath.replace(root, '').startsWith('/functions/')) {
+          const filePath = fullPath.replace(/\.[jt]sx?$/i, '')
+          cacheBust.set(filePath, (cacheBust.get(filePath) || 0) + 1)
+        }
+      })
+
       async function handleFunctionRequest(ctx, functionPath) {
         try {
-          const filePath = root + '/functions' + functionPath + '.js'
-          const cacheBust = `?cacheBust=${Math.random().toString(36).substr(2)}`
-          let endpointMeta = await import(filePath + cacheBust)
+          const filePath = root + '/functions' + functionPath
+          const cacheBust = `?cacheBust=${cacheBust.get(filePath) || 0}`
+          let endpointMeta = await import(filePath + '.js' + cacheBust)
 
           if (endpointMeta) {
             endpointMeta = endpointMeta.default || endpointMeta
