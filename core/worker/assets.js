49bdc485da5b091037b23f693f3e50fac4cdb03f
Fran Dios <frankdiox@gmail.com>
2020-11-29 04:48:55 +0900

    Add more asset matchers

--- core/worker/assets.js
+++ core/worker/assets.js
@@ -3,7 +3,9 @@ import { getAssetFromKV } from '@cloudflare/kv-asset-handler'
 export function isStaticAsset(event) {
   return (
     event.request.url.includes('/_assets/') ||
-    event.request.url.includes('/favicon.ico')
+    event.request.url.includes('/favicon.ico') ||
+    event.request.url.includes('/robots.txt') ||
+    event.request.url.includes('/sitemap.xml')
   )
 }
 
