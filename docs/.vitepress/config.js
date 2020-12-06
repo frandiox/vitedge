9997e189c16e6a926499a7d7b4b21ce9adfa4202
Fran Dios <frankdiox@gmail.com>
2020-12-06 19:38:02 +0900

    docs: Render docs using VitePress

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  title: 'Vitedge',
+  description: 'Edge-side rendering and fullstack Vite framework',
+  themeConfig: {
+    sidebar: [
+      {
+        text: 'Getting Started',
+        link: '/getting-started',
+      },
+      {
+        text: 'Usage',
+        link: '/usage',
+      },
+      {
+        text: 'Page Props',
+        link: '/props',
+      },
+      {
+        text: 'API',
+        link: '/api',
+      },
+      {
+        text: 'Cache',
+        link: '/cache',
+      },
+      {
+        text: 'Troubleshooting',
+        link: '/troubleshooting',
+      },
+    ],
+  },
+}
---
+++ docs/index.md
@@ -0,0 +1,17 @@
+---
+home: true
+# heroImage: /logo.png
+heroAlt: Logo image
+heroText: Vitedge
+tagline: Edge-side rendering and fullstack Vite framework
+actionText: Get Started
+actionLink: /getting-started
+features:
+  - title: Just a Vite app™
+    details: Powered by Vite, provides ultrafast HMR and great DX.
+  - title: Edge side redering
+    details: Rendering performed in CDN nodes. Cached at the edge.
+  - title: Fullstack
+    details: Generate API endpoints automatically from filesystem routes.
+footer: MIT Licensed | Copyright © 2020-present Fran Dios
+---
