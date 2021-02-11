2ae9c4f81c564d1c6f6a398cda22a1bd76d715b9
Fran Dios <frankdiox@gmail.com>
2021-02-11 23:38:46 +0900

    feat: Add default browser cache-age to assets

--- core/worker/assets.js
+++ core/worker/assets.js
@@ -8,6 +8,20 @@ export function isStaticAsset(event) {
   )
 }
 
-export function handleStaticAsset(event) {
-  return getAssetFromKV(event, {})
+export async function handleStaticAsset(event) {
+  const response = await getAssetFromKV(event, {})
+
+  if (response.status < 400) {
+    const url = new URL(event.request.url)
+    const filename = url.pathname.split('/').pop()
+
+    const maxAge =
+      filename.split('.').length > 2
+        ? 31536000 // hashed asset, will never be updated
+        : 86400 // favico and other public assets
+
+    response.headers.append('cache-control', `public, max-age=${maxAge}`)
+  }
+
+  return response
 }
