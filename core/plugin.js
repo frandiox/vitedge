2c1cc66de230aab05e0459948a2dee14af82088b
Fran Dios <frankdiox@gmail.com>
2021-02-15 20:24:16 +0900

    fix: Support vite@2.0.0-beta.69

--- core/plugin.js
+++ core/plugin.js
@@ -5,7 +5,8 @@ export default () => {
     name: 'vitedge',
     configureServer,
     configResolved: (config) => {
-      config.alias.push({
+      // Vite-beta>=69 wraps 'alias' in 'resolve'
+      ;(config.resolve.alias || config.alias).push({
         find: /^vitedge$/,
         replacement: config.build.ssr
           ? 'vitedge/entry-server'
