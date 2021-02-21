f3bc552d5548f980661b48a7694424977e965db6
Fran Dios <frankdiox@gmail.com>
2021-02-22 00:37:58 +0900

    feat: Make worker's webpack config a function to support options

--- core/webpack.cjs
+++ core/webpack.cjs
@@ -1,20 +1,15 @@
 const path = require('path')
 const { rootDir, outDir, ssrOutDir, fnsOutFile } = require('./config.cjs')
 
-module.exports = {
+module.exports = ({ root = rootDir } = {}) => ({
   entry: './index',
   target: 'webworker',
   resolve: {
     mainFields: ['main', 'module'],
     alias: {
-      __vitedge_functions__: path.resolve(rootDir, outDir, fnsOutFile),
-      __vitedge_router__: path.resolve(rootDir, outDir, ssrOutDir),
-      __vitedge_meta__: path.resolve(
-        rootDir,
-        outDir,
-        ssrOutDir,
-        'package.json'
-      ),
+      __vitedge_functions__: path.resolve(root, outDir, fnsOutFile),
+      __vitedge_router__: path.resolve(root, outDir, ssrOutDir),
+      __vitedge_meta__: path.resolve(root, outDir, ssrOutDir, 'package.json'),
     },
   },
-}
+})
