bd74b45eaf54625d61cd6df4729a8d2c61f0e9ed
Fran Dios <frankdiox@gmail.com>
2021-04-24 02:44:22 +0900

    refactor: Move code to a separate file

---
+++ src/build/env.js
@@ -0,0 +1,26 @@
+import { loadEnv } from '../utils/env.js'
+
+export function resolveEnvVariables({ mode }) {
+  const actualMode = mode || process.env.NODE_ENV || 'production'
+
+  const envVariables = loadEnv({
+    mode: actualMode,
+    dry: true,
+  })
+
+  const nodeEnv = envVariables.NODE_ENV || process.env.NODE_ENV || actualMode
+
+  return {
+    'process.env.MODE': JSON.stringify(actualMode),
+    'process.env.NODE_ENV': JSON.stringify(nodeEnv),
+    ...Object.entries(envVariables).reduce(
+      (acc, [key, value]) => ({
+        ...acc,
+        [`process.env.${key}`]: JSON.stringify(value),
+      }),
+      {}
+    ),
+    'process.env.': `({}).`,
+    'process.env': JSON.stringify(envVariables),
+  }
+}
--- src/build/functions.js
+++ src/build/functions.js
@@ -6,44 +6,23 @@ import { nodeResolve } from '@rollup/plugin-node-resolve'
 import json from '@rollup/plugin-json'
 import commonjs from '@rollup/plugin-commonjs'
 import replace from '@rollup/plugin-replace'
-import { loadEnv } from '../utils/env.js'
+import { resolveEnvVariables } from './env.js'
 
 function resolveFiles(globs, extensions) {
   return fg(
     globs.map((glob) => `${glob}.{${extensions.join(',')}}`),
     {
-      ignore: ['node_modules', '.git'],
+      ignore: ['node_modules', '.git', '**/index.*'],
       onlyFiles: true,
     }
   )
 }
 
-function resolveEnvVariables({ mode }) {
-  const actualMode = mode || process.env.NODE_ENV || 'production'
-
-  const envVariables = loadEnv({
-    mode: actualMode,
-    dry: true,
-  })
-
-  const nodeEnv = envVariables.NODE_ENV || process.env.NODE_ENV || actualMode
-
-  return {
-    'process.env.MODE': JSON.stringify(actualMode),
-    'process.env.NODE_ENV': JSON.stringify(nodeEnv),
-    ...Object.entries(envVariables).reduce(
-      (acc, [key, value]) => ({
-        ...acc,
-        [`process.env.${key}`]: JSON.stringify(value),
-      }),
-      {}
-    ),
-    'process.env.': `({}).`,
-    'process.env': JSON.stringify(envVariables),
-  }
-}
-
-export default async function ({ mode, fnsInputPath, fnsOutputPath }) {
+export default async function buildFunctions({
+  mode,
+  fnsInputPath,
+  fnsOutputPath,
+}) {
   const fnsRoutes = await resolveFiles(
     [
       fnsInputPath + '/*',
@@ -71,7 +50,10 @@ export default async function ({ mode, fnsInputPath, fnsOutputPath }) {
     input: 'entry',
     plugins: [
       virtual({ entry: virtualEntry }),
-      replace(resolveEnvVariables({ mode })),
+      replace({
+        values: resolveEnvVariables({ mode }),
+        preventAssignment: true,
+      }),
       esbuild(),
       nodeResolve({
         preferBuiltins: false,
