291c9f8ed8878ec8a04344f3ac4bd2bf9b99adc4
Fran Dios <frankdiox@gmail.com>
2021-07-18 21:23:01 +0200

    feat: Make environment variable handling more similar to Vite

--- src/utils/env.js
+++ src/utils/env.js
@@ -1,13 +1,24 @@
 import path from 'path'
-import multienv from 'multienv-loader'
+import { loadEnv as viteLoadEnv } from 'vite'
 import { getProjectInfo, meta } from '../config.js'
 
-export async function loadEnv({ mode = 'development', dry = true } = {}) {
-  const { rootDir } = await getProjectInfo()
-  return multienv.load({
-    filter: (key) => key.startsWith('VITEDGE_') || key === 'NODE_ENV',
-    envPath: path.resolve(rootDir, meta.fnsInDir),
-    mode,
-    dry,
-  })
+export async function loadEnv({ mode = 'development', dry = true, root } = {}) {
+  if (!root) {
+    const info = await getProjectInfo()
+    root = path.resolve(info.rootDir, meta.fnsInDir)
+  }
+
+  const env = await viteLoadEnv(mode, root, 'VITEDGE_')
+  env.MODE = mode
+  env.NODE_ENV = process.env.VITE_USER_NODE_ENV || process.env.NODE_ENV || mode
+  env.PROD = env.NODE_ENV === 'production'
+  env.DEV = !env.PROD
+
+  if (!dry) {
+    for (const [key, value] of Object.entries(env)) {
+      process.env[key] = value
+    }
+  }
+
+  return env
 }
