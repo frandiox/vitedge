7c828e8ef9c0cf4706b1dbd2d2b5cb8f6a1e2d3e
Fran Dios <frankdiox@gmail.com>
2021-07-22 19:08:38 +0200

    feat: Support import.meta.env in Functions

--- src/dev/js-loader.js
+++ src/dev/js-loader.js
@@ -1,6 +1,14 @@
-import { cacheBust } from './cache-bust.js'
+import { cacheBust, transformEnvStatements } from './loader-utils.js'
 
 export const resolve = async (specifier, context, defaultResolve) => {
   const resolved = defaultResolve(specifier, context, defaultResolve)
   return cacheBust(resolved)
+}
+
+export const transformSource = (source, context, defaultTransformSource) => {
+  return defaultTransformSource(
+    transformEnvStatements(source, context),
+    context,
+    defaultTransformSource
+  )
 }
--- src/dev/ts-loader.js
+++ src/dev/ts-loader.js
@@ -1,8 +1,20 @@
-export { getFormat, transformSource } from 'ts-node/esm'
-import { resolve as tsResolve } from 'ts-node/esm'
-import { cacheBust } from './cache-bust.js'
+export { getFormat } from 'ts-node/esm'
+
+import {
+  resolve as tsResolve,
+  transformSource as tsTransformSource,
+} from 'ts-node/esm'
+import { cacheBust, transformEnvStatements } from './loader-utils.js'
 
 export const resolve = async (specifier, context, defaultResolve) => {
   const resolved = await tsResolve(specifier, context, defaultResolve)
   return cacheBust(resolved)
+}
+
+export const transformSource = (source, context, defaultTransformSource) => {
+  return tsTransformSource(
+    transformEnvStatements(source, context),
+    context,
+    defaultTransformSource
+  )
 }
