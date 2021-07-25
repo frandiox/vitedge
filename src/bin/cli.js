5589173ef97eb32ea925275afd82e945b1c57b4d
Fran Dios <frankdiox@gmail.com>
2021-07-25 19:16:38 +0200

    fix: Parsing CLI options

--- src/bin/cli.js
+++ src/bin/cli.js
@@ -4,23 +4,26 @@ import cp from 'child_process'
 
 const [, , ...args] = process.argv
 
-const onlyStringArgs = ['ssr', 'mode', 'entry']
+function parseOptions({ onlyStringArgs = [] } = {}) {
+  const options = {}
+  for (let i = 0; i < args.length; i++) {
+    const arg = args[i]
+    const nextArg = args[i + 1]
+    if (arg.startsWith('--')) {
+      const isBoolean = !nextArg || nextArg.startsWith('--')
+      const argName = arg.replace('--', '')
+      if (isBoolean && onlyStringArgs.includes(argName)) {
+        throw new Error(
+          `Value of argument "${argName}" must be a string. E.g. ${arg} <string>`
+        )
+      }
 
-const options = {}
-for (let i = 0; i < args.length; i++) {
-  const arg = args[i]
-  const nextArg = args[i + 1]
-  if (arg.startsWith('--')) {
-    const isBoolean = !nextArg || nextArg.startsWith('--')
-    const argName = arg.replace('--', '')
-    if (isBoolean && onlyStringArgs.includes(argName)) {
-      throw new Error(
-        `Value of argument "${argName}" must be a string. E.g. ${arg} <string>`
-      )
+      options[argName.replace(/-([a-z])/i, (_, s1) => s1.toUpperCase())] =
+        isBoolean ? true : nextArg
     }
-
-    options[argName] = isBoolean ? true : nextArg
   }
+
+  return options
 }
 
 const [command] = args
@@ -28,13 +31,9 @@ const [command] = args
 ;(async () => {
   if (command === 'build') {
     const { default: build } = await import('vitedge/build/index.js')
+    const options = parseOptions({ onlyStringArgs: ['ssr', 'mode', 'entry'] })
 
-    await build({
-      mode: options.mode,
-      ssr: options.ssr,
-      entry: options.entry,
-      watch: !!options.watch,
-    })
+    await build(options)
 
     if (!options.watch) {
       process.exit()
@@ -44,6 +43,8 @@ const [command] = args
     command === undefined ||
     command.startsWith('-')
   ) {
+    const options = parseOptions({ onlyStringArgs: ['mode'] })
+
     if (options.ssr) {
       if (typeof options.ssr !== 'string') {
         // Remove --ssr if there is no path specified
