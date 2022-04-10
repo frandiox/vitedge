c1a086465717e836bb93e94104545cabacd49734
Fran Dios <fran.dios@shopify.com>
2022-04-10 19:14:28 +0200

    fix: illegal invocation in CFW

--- src/worker/index.js
+++ src/worker/index.js
@@ -43,7 +43,10 @@ export async function handleEvent(
     )
   }
 
-  globalThis.fetch = createLocalFetch(event.request, event.waitUntil)
+  globalThis.fetch = createLocalFetch(
+    event.request,
+    event.waitUntil.bind(event)
+  )
 
   // --- PROPS ENDPOINTS
   if (isPropsRequest(event)) {
